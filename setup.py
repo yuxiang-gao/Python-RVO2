a5fdb5724f7fbb9b5e326ee043dd3d0f00682f8c
Yuxiang Gao <yuxiang.gao@jhu.edu>
2021-08-04 17:46:52 -0400

    Add install requirements in setup

--- setup.py
+++ setup.py
@@ -12,40 +12,45 @@ class BuildRvo2Ext(_build_ext):
         import os.path
         import subprocess
 
-        build_dir = os.path.abspath('build/RVO2')
+        build_dir = os.path.abspath("build/RVO2")
         if not os.path.exists(build_dir):
             os.makedirs(build_dir)
-            subprocess.check_call(['cmake', '../..', '-DCMAKE_CXX_FLAGS=-fPIC'],
-                                  cwd=build_dir)
-        subprocess.check_call(['cmake', '--build', '.'], cwd=build_dir)
+            subprocess.check_call(
+                ["cmake", "../..", "-DCMAKE_CXX_FLAGS=-fPIC"], cwd=build_dir
+            )
+        subprocess.check_call(["cmake", "--build", "."], cwd=build_dir)
 
         _build_ext.run(self)
 
 
 extensions = [
-    Extension('rvo2', ['src/*.pyx'],
-              include_dirs=['src'],
-              libraries=['RVO'],
-              library_dirs=['build/RVO2/src'],
-              extra_compile_args=['-fPIC']),
+    Extension(
+        "rvo2",
+        ["src/*.pyx"],
+        include_dirs=["src"],
+        libraries=["RVO"],
+        library_dirs=["build/RVO2/src"],
+        extra_compile_args=["-fPIC"],
+    ),
 ]
 
 setup(
     name="pyrvo2",
     ext_modules=cythonize(extensions),
-    cmdclass={'build_ext': BuildRvo2Ext},
+    cmdclass={"build_ext": BuildRvo2Ext},
+    install_requires=["cython"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Information Technology',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Cython',
-        'Topic :: Games/Entertainment :: Simulation',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Cython",
+        "Topic :: Games/Entertainment :: Simulation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
